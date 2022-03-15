75003a93e0903bf304e66b3ab76ab4f8e96d4cef
Bekzod1306 <1999bekzod3772@gmail.com>
2022-03-15 23:08:11 +0500

    php freamwork for projects

---
+++ vendor/freamwork/core/base/Model.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ishop\base;
+use ishop\Db;
+class Model
+{
+    public $attributes = [];
+    public $errors = [];
+    public $rules = [];
+
+    public function __construct(){
+        Db::instance();
+    }
+
+}
